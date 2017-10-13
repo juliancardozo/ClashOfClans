e88cc7e6a091c320859a0eb7491a88762e03e938
elianalombardo <31491867+elianalombardo@users.noreply.github.com>
2017-10-13 20:58:42 -0300

    Factory

---
+++ ClashOfClans/FactoryEdificio/FactoryCampamento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClashOfClans
+{
+    class FactoryCampamento: IFactoryEdificio
+    {
+    
+        public IEdificio Instanciar(Aldea aldea)
+        {
+            return new Campamento(aldea);
+        }
+    }
+}
