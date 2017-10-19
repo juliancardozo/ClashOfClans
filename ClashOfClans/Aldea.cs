c98283451a1ac12684f8b3394726a6e5932e93d9
Matias Rodriguez Lima <matias.rodriguez.lima@gmail.com>
2017-10-18 21:41:37 -0300

    Avance aldea

--- ClashOfClans/Aldea.cs
+++ ClashOfClans/Aldea.cs
@@ -17,53 +17,14 @@ namespace ClashOfClans
         public int AlmacenElixirRojo { get => almacenElixirRojo; set => almacenElixirRojo = value; }
         public int AlmacenElixirNegro { get => almacenElixirNegro; set => almacenElixirNegro = value; }
 
-        List<Canon> Canones;
-        List<Antiaereo> Antiaereos;
-        List<TorreMago> TorresMago;
-        List<TorreArquera> TorresArquera;
-        List<TorreInfierno> TorresInfierno;
-        List<Muro> Muros;
-        List<Tesla> Teslas;
-        List<Aguila> Aguilas;
-        List<Ballesta> Ballestas;
-        List<Cuartel> Cuarteles;
-        List<CuartelOscuro> CuartelesOscuro;
+        List<IEdificio> Edificios;
         readonly Ayuntamiento Ayuntamiento;
         readonly Castillo Castillo;
-        List<Campamento> Campamentos;
-        List<AlmacenOro> AlmacenesOro;
-        List<AlmacenElixirRojo> AlmacenesElixirRojo;
-        List<AlmacenElixirNegro> AlmacenesElixirNegro;
-        List<RecolectoraOro> RecolectorasOro;
-        List<RecolectoraElixirRojo> RecolectorasElixirRojo;
-        List<RecolectoraElixirNegro> RecolectorasElixirNegro;
 
+        List<ITropa> Tropas;
         readonly Warden Warden;
         readonly Rey Rey;
         readonly Reina Reina;
-
-        // Normales
-        List<Barbaro> Barbaros;
-        List<Arquera> Arqueras;
-        List<Gigante> Gigantes;
-        List<Mago> Magos;
-        List<Curandera> Curanderas;
-        List<DragonBebe> DragonesBebe;
-        List<Dragon> Dragones;
-        List<Minero> Mineros;
-        List<Pekka> Pekkas;
-        List<Globo> Globos;
-        List<Duende> Duendes;
-        List<Esqueleto> Esqueletos;
-
-        // Oscuras
-        List<Esbirro> Esbirros;
-        List<MontaPuerco> MontasPuerco;
-        List<Golem> Golems;
-        List<Valkiria> Valkirias;
-        List<Bruja> Brujas;
-        List<LanzaRoca> LanzaRocas;
-        List<Lava> Lavas;
 
         Dictionary<String, IFactory> constructores;
 
@@ -74,51 +35,29 @@ namespace ClashOfClans
             almacenElixirRojo = 800;
             almacenElixirNegro = 500;
 
-            Canones = new List<Canon>();
-            Antiaereos = new List<Antiaereo>();
-            TorresMago = new List<TorreMago>();
-            TorresArquera = new List<TorreArquera>();
-            TorresInfierno = new List<TorreInfierno>();
-            Muros = new List<Muro>();
-            Teslas = new List<Tesla>();
-            Aguilas = new List<Aguila>();
-            Ballestas = new List<Ballesta>();
-            Cuarteles = new List<Cuartel>();
-            CuartelesOscuro = new List<CuartelOscuro>();
-            Campamentos = new List<Campamento>();
-            AlmacenesOro = new List<AlmacenOro>();
-            AlmacenesElixirRojo = new List<AlmacenElixirRojo>();
-            AlmacenesElixirNegro = new List<AlmacenElixirNegro>();
-            RecolectorasOro = new List<RecolectoraOro>();
-            RecolectorasElixirRojo = new List<RecolectoraElixirRojo>();
-            RecolectorasElixirNegro = new List<RecolectoraElixirNegro>();
-
-            // Normales
-            Barbaros = new List<Barbaro>();
-            Arqueras = new List<Arquera>();
-            Gigantes = new List<Gigante>();
-            Magos = new List<Mago>();
-            Curanderas = new List<Curandera>();
-            DragonesBebe = new List<DragonBebe>();
-            Dragones = new List<Dragon>();
-            Mineros = new List<Minero>();
-            Pekkas = new List<Pekka>();
-            Mineros = new List<Minero>();
-            Globos = new List<Globo>();
-            Duendes = new List<Duende>();
-            Esqueletos = new List<Esqueleto>();
-
-            // Oscuras
-            Esbirros = new List<Esbirro>();
-            MontasPuerco = new List<MontaPuerco>();
-            Golems = new List<Golem>();
-            Valkirias = new List<Valkiria>();
-            Brujas = new List<Bruja>();
-            LanzaRocas = new List<LanzaRoca>();
-            Lavas = new List<Lava>();
+            Edificios = new List<IEdificio>();
+            Tropas = new List<ITropa>();
 
             constructores = new Dictionary<string, IFactory>;
             constructores.Add("Arquera", new FactoryArquera(this));
+
+        }
+
+        public void crearTropa(String tropa)
+        {
+            ITropa t = constructores[tropa].Instanciar();
+            // Controlo espacios disponibles
+            bool espacioDisponible = false;
+            Int32 totalEspaciosDisponibles = 0;
+            foreach(IEdificio edificio in Edificios)
+            {
+
+            }
+
+
+            // Controlo recursos disponibles
+            bool recursosDisponible = false;
+
 
         }
 
