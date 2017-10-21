ef1f35f5525a1dd7590beda90300d8a388aba6f0
julian_cardozo <julian@pambumedia.com>
2017-10-20 23:34:48 -0300

    Proyecto Final Entregado

--- ClashOfClans/Form1.Designer.cs
+++ ClashOfClans/Form1.Designer.cs
@@ -1077,6 +1077,7 @@
             this.Controls.Add(this.groupBox1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
+            this.Text = "Clash of clans";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.groupBox1.ResumeLayout(false);
